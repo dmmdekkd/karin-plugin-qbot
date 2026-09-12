1d8e71d9ec2fc5b56a80fc9defdd32a45c33321b
dmmdekkd <3448585471@qq.com>
2026-09-12 20:54:14 +0800

    refactor(index): 初始化日志改为插件名+版本+耗时格式

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,8 @@ import { dir } from './dir'
 import { pluginVersion } from './utils/version'
 import { logger } from 'node-karin'
 
+/** 记录模块加载开始时间，用于输出初始化耗时 */
+const startTime = performance.now()
+
 /** 请不要在这编写插件 不会有任何效果~ */
-logger.info(`${logger.violet(`[插件:${pluginVersion}]`)} ${logger.green(dir.name)} 初始化完成~`)
+logger.info(`${logger.violet(`[插件:${dir.name}]`)} ${logger.green(`v${pluginVersion}`)} 初始化完成 ~ 耗时 ${logger.green(`${Math.round(performance.now() - startTime)}ms`)}`)
